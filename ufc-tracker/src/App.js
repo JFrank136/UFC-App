21d88a4d4df8d5a2d37a764c530986c0268aa4f7
JFrank136 <billlanders247@gmail.com>
2025-05-18 21:40:53 -0400

    css

--- ufc-tracker/src/App.js
+++ ufc-tracker/src/App.js
@@ -1,34 +1,31 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import FighterSearch from "./pages/FighterSearch";
-import UpcomingFights from "./pages/UpcomingFights";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import FighterSearch from './pages/FighterSearch';
+import UpcomingFights from './pages/UpcomingFights';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<FighterSearch />} />
-      </Routes>
-    </Router>
-  );
-}
-
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <nav>
-          <ul>
-            <li><Link to="/">Upcoming Fights</Link></li>
-            <li><Link to="/search">Search Fighters</Link></li>
+        <nav className="p-4 bg-gray-100 shadow-sm mb-4">
+          <ul className="flex space-x-4 justify-center">
+            <li>
+              <Link to="/" className="text-blue-600 hover:underline">
+                Upcoming Fights
+              </Link>
+            </li>
+            <li>
+              <Link to="/search" className="text-blue-600 hover:underline">
+                Search Fighters
+              </Link>
+            </li>
           </ul>
         </nav>
+
         <Routes>
           <Route path="/" element={<UpcomingFights />} />
-          <Route path="/" element={<FighterSearch />} />
+          <Route path="/search" element={<FighterSearch />} />
         </Routes>
       </div>
     </BrowserRouter>
